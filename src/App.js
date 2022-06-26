45726b2eca879ef0fe4ddd894857dfd04fc79beb
Abbas <abbasrizvi200209@gmail.com>
2022-06-26 16:27:59 +0530

    abbas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
 
-
-
 function App() {
   return (
-    <Login />
+    <div>
+      <Login />
+    </div>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,54 +1,48 @@
 import React from 'react'
 import './Login.css'
-import GoogleIcon from '@mui/icons-material/Google';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Login() {
-  return (
-    <div className='loginContainer'>
-    <div className='Content'>
-        <div className='Text'>
-            <h2>Welcome back</h2>
+    return (
+        <div className='LoginContanier'>
+            <div className='LoginPage'>
+                <div className='UpperText'>
+                    <h2>Welcome Back</h2>
+                </div>
+                <div className='FirstInput'>
+                    <text>Email</text>
+                    <input placeholder='Email'></input>
+                </div>
+                <div className='SecondInput'>
+                    <text>Password</text>
+                    <input placeholder='Password'></input>
+                </div>
+                <div className='button'>
+                    <div className='buttontext'>
+                        <text>Forget Password?</text>
+                    </div>
+                    <button>Login</button>
+                </div>
+                <div className='OrText'>
+                    <text>------ OR -----</text>
+                    <div className='AllImage'>
+                        <div className='image'>
+                            <img className='LogoImage1' src='https://cdn-icons-png.flaticon.com/512/300/300221.png' alt='googel' />
+                        </div>
+                        <div>
+                            <img className='LogoImage2' src=' https://cdn-icons-png.flaticon.com/512/733/733547.png' alt="Abbas" />
+                        </div>
+                        <div>
+                            <img className='LogoImage3' src='https://cdn-icons-png.flaticon.com/512/733/733579.png' alt='google' />
+                        </div>
+                        <div>
+                            <img className='LogoImage4' src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt='google' />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
-    <div className='Object'>
-        <div className='FirstInput'>
-            <text>Email</text>
-            <input></input>
-        </div>
-        <div className='SecondInput'>
-        <text>Password</text>
-            <input></input>
-        </div>
-        <div className='Button'>
-        <text>Forget Password?</text>
-            <button>Login</button>
-        </div>
-        <div className='text'>
-       <text>OR</text>
-       </div>
-       <div className='string'>
-        <text>join saasguru with your favorite social media account</text>
-       </div>
-       <div className='LogoIcon'>
-       <div className='google'>
-       <GoogleIcon />
-       </div>
-       <div className='google'>
-        <InstagramIcon />
-       </div>
-       <div className='google'>
-        <FacebookIcon />
-       </div>
-       <div className='google'>
-        <LinkedInIcon />
-       </div>
-        </div>
-        </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Login
